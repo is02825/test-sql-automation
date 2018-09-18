2125805e8d19fe39f27fc9f2d4a052467e6e316e
unknown <is92825@SECURA-AD>
2018-09-18 16:02:56 -0500

    removed redudant table and created releationship

--- Tables/test.DbaandDeveloperLikes.sql
+++ Tables/test.DbaandDeveloperLikes.sql
@@ -4,5 +4,7 @@ CREATE TABLE [test].[DbaandDeveloperLikes]
 [developerID] [tinyint] NOT NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [test].[DbaandDeveloperLikes] ADD CONSTRAINT [PK__DbaandDe__8399E141DE6E4886] PRIMARY KEY CLUSTERED  ([dbaID]) ON [PRIMARY]
+ALTER TABLE [test].[DbaandDeveloperLikes] ADD CONSTRAINT [FK_DbaandDeveloperLikes_DBAs] FOREIGN KEY ([dbaID]) REFERENCES [test].[DBAs] ([dbaID])
+GO
+ALTER TABLE [test].[DbaandDeveloperLikes] ADD CONSTRAINT [FK_DbaandDeveloperLikes_Developers] FOREIGN KEY ([developerID]) REFERENCES [test].[Developers] ([developerID])
 GO
