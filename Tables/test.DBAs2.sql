ba26e59a5dc6a9595cc07e49d37f3d6ef0b7b61e
unknown <is92825@SECURA-AD>
2018-12-03 14:53:20 -0600

    Showing SRG

---
+++ Tables/test.DBAs2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [test].[DBAs2]
+(
+[dbaID] [tinyint] NOT NULL IDENTITY(1, 1),
+[dbaFirstName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[dbaLastName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [test].[DBAs2] ADD CONSTRAINT [PK_DBAs2] PRIMARY KEY CLUSTERED  ([dbaID]) ON [PRIMARY]
+GO
